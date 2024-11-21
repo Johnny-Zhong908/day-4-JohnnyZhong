61f71067d2161b36be56989f8948d173612ffa60
My Name <myEmail@example.com>
2024-11-21 20:33:37 +0800

    step3

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,26 @@
 package oo;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Klass klass = (Klass) obj;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(number);
+    }
 }
--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,13 +1,28 @@
 package oo;
 
 public class Student extends Person {
+    private Klass klass;  // The class the student belongs to
+
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
-    @Override
-    public String introduce() {
-        return super.introduce() + " I am a student.";
+    // Method to set or change the class the student belongs to
+    public void join(Klass klass) {
+        this.klass = klass;
     }
 
+    // Method to check if the student belongs to a particular class
+    public boolean isIn(Klass klass) {
+        return this.klass != null && this.klass.equals(klass);
+    }
+
+    @Override
+    public String introduce() {
+        String introduction = super.introduce() + " I am a student.";
+        if (klass != null) {
+            introduction += " I am in class " + klass.getNumber() + ".";
+        }
+        return introduction;
+    }
 }
