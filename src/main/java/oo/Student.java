9b488427d380106526dfb59b223af21f76a7c2c5
My Name <myEmail@example.com>
2024-11-21 20:36:01 +0800

    step3

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,18 +1,14 @@
 package oo;
 
 public class Student extends Person {
-    private Klass klass;  // The class the student belongs to
+    private Klass klass;
 
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
-
-    // Method to set or change the class the student belongs to
     public void join(Klass klass) {
         this.klass = klass;
     }
-
-    // Method to check if the student belongs to a particular class
     public boolean isIn(Klass klass) {
         return this.klass != null && this.klass.equals(klass);
     }
